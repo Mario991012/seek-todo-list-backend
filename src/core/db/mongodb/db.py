fcdeefae12a293329c1500ebfa0c869d06d1e71e
Mario Velasquez <mvelasquezb99@gmail.com>
2025-03-27 00:44:41 -0600

    feat(mongo-config): added mongodb config and task model

---
+++ src/core/db/mongodb/db.py
@@ -0,0 +1,11 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+client = AsyncIOMotorClient(MONGODB_URI)
+db = client.seekTodoList
+tasks_collection = db.tasks
