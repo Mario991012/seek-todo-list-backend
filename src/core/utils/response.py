bba0ba2381c7c51b23c29e8a04825aadb0e1cd01
Mario Velasquez <mvelasquezb99@gmail.com>
2025-03-27 03:15:28 -0600

    test(tasks): tests for services added

--- src/core/utils/response.py
+++
@@ -1,9 +0,0 @@
-from src.core.common.return_codes import ReturnCode
-
-def format_response(return_code: ReturnCode, data: dict = None):
-    if data is None:
-        data = {}
-    return {
-        "return_code": return_code.value,
-        "data": data
-    }
