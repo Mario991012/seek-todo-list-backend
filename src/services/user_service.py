d1abef7e1e8d73279ddd33337d07a944d2d2c973
Mario Velasquez <mvelasquezb99@gmail.com>
2025-03-27 22:25:26 -0600

    chore(auth): Added user model and user service to authenticate

---
+++ src/services/user_service.py
@@ -0,0 +1,26 @@
+from passlib.context import CryptContext
+from src.core.services.mongodb_service import MongoDBService
+from src.core.db.mongodb.models.user import User
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class UserService:
+    def __init__(self, user_db_service: MongoDBService):
+        self.user_db_service = user_db_service
+
+    async def create_user(self, user: User):
+        existing_user = await self.user_db_service.get({"username": user.username}, 1)
+        if existing_user:
+            return None
+        
+        hashed_password = pwd_context.hash(user.password)
+        user_data = {"username": user.username, "password": hashed_password}
+        await self.user_db_service.create(user_data)
+        return user_data
+
+    async def authenticate_user(self, username: str, password: str):
+        users = await self.user_db_service.get({"username": username}, 1)
+        user = users[0]
+        if user and pwd_context.verify(password, user["password"]):
+            return user
+        return None
