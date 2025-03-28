7ad352f737a4d9e2e74083b6bb698d8bf2afb114
Mario Velasquez <mvelasquezb99@gmail.com>
2025-03-28 06:03:51 -0600

    fix(cdk): cors issue fixed

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from src.api.v1.routes.tasks import taskRouter
 from src.api.v1.routes.auth import authRouter
-from mangum import Mangum
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(taskRouter, prefix="/v1/tasks", tags=["tasks"])
 app.include_router(authRouter, prefix="/v1/auth", tags=["auth"])
-
-handler = Mangum(app)
